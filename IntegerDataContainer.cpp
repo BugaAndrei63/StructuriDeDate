9d31e3f667362ea477858270ff79572812d525e2
BugaAndrei63 <72763756+BugaAndrei63@users.noreply.github.com>
2021-04-12 22:52:26 +0300

    Added more features
    Added the delete element, min, max, k-element, succesor, predecesor and "is inside" features

--- IntegerDataContainer.cpp
+++ IntegerDataContainer.cpp
@@ -5,7 +5,6 @@ using namespace std;
 struct element
 {
     int value;
-
     int index;
 
     element * next;
@@ -27,7 +26,9 @@ class idc //integer data container
          cardinal = 0;
     }
 
-    void insert(int val)
+    ///FUNCTIONS///
+    
+    void insert(int value)
     {
         element * new_element = new element;
 
@@ -35,11 +36,12 @@ class idc //integer data container
         {
             new_element->next = last;
             new_element->prev = nullptr;
-            new_element->value = val;
+            new_element->value = value;
             new_element->index = 0;
 
             first = new_element;
             last = new_element;
+
         }else
         {
             
@@ -47,7 +49,7 @@ class idc //integer data container
 
             new_element->next = nullptr;
             new_element->prev = last;
-            new_element->value = val;
+            new_element->value = value;
             new_element->index = cardinal + 1;
 
             cardinal++;
@@ -58,31 +60,268 @@ class idc //integer data container
 
     }
 
+    bool index_check(int index)
+    {
+        if(index > cardinal || index < 0)
+        {
+            cout<<"Invalid index"<<endl;
+            return 0;
+        }
+        return 1;
+    }
+
+    void del(int index)
+    {
+        if(index_check(index) != 0)
+        {
+            int i = 0;
+            while(i <= cardinal)
+            {
+                
+                if(i == index)
+                {
+                    element * temp_prev = (*this)[i]->prev;
+                    element * temp_next = (*this)[i]->next;
+
+                    if(temp_prev != nullptr)
+                    {
+                        temp_prev->next = temp_next;
+                    }
+                    else
+                    {
+                        first = temp_next;
+                    }
+
+                    if(temp_next != nullptr)
+                    {
+                        temp_next->prev = temp_prev;
+                        element * temp = temp_next;
+                        while(temp->next != nullptr)
+                        {
+                            temp->index -=1;
+                            temp = temp->next;
+                        }
+                        temp->index -=1;
+                    }else
+                    {
+                        last = temp_prev;
+                    }
+                    cardinal -= 1;
+                    
+                    break;            
+                }
+                i++;
+            }
+        }
+     
+    }
+
+    element* min()
+    {
+        element * min = first;
+
+        for(int i = 0; i<= cardinal; i++)
+        {
+            if((*this)[i]->value < min->value)
+            {
+                min = (*this)[i];
+            }
+        }
+
+        return min;
+    }
+
+    element* max()
+    {
+        element * max = first;
+
+        for(int i = 0; i<= cardinal; i++)
+        {
+            if((*this)[i]->value > max->value)
+            {
+                max = (*this)[i];
+            }
+        }
+
+        return max;
+    }
+
+    element* succesor(int index)
+    {
+        if(index_check(index) != 0)
+        {
+            int difference = (*this)[index]->value - (*this).min()->value;
+            int pos = -1;
+            for(int i = 0; i <= cardinal ; i++)
+            {
+                
+                if((*this)[i] != (*this)[index] && (*this)[i]->value > (*this)[index]->value &&abs((*this)[i]->value - (*this)[index]->value) < difference)
+                {
+                    difference = abs((*this)[i]->value - (*this)[index]->value);
+                    pos = i;
+                }
+            }
+            if(pos == -1)
+            {
+                cout<<"No succesors.";
+            }else
+            {
+                return (*this)[pos];
+            }
+        }else
+        {
+            cout<<"Null";
+            return nullptr;
+        }
+
+        
+        
+    }
+
+    element* predecessor(int index)
+    {
+       if(index_check(index) != 0)
+       {
+            int difference = abs((*this)[index]->value - (*this).min()->value);
+            int pos = -1;
+            for(int i = 0; i <= cardinal ; i++)
+            {
+                
+                if((*this)[i] != (*this)[index] && (*this)[i]->value < (*this)[index]->value && abs((*this)[index]->value - (*this)[i]->value) <= difference)
+                {
+                    difference = abs((*this)[index]->value - (*this)[i]->value);
+                    pos = i;
+                }
+            }
+            if(pos == -1)
+            {
+                cout<<"No predecesors.";
+            }else
+            {
+                return (*this)[pos];
+            }
+       }else
+        {
+            cout<<"Null";
+            return nullptr;
+        }
+
+        
+    }
+
+    element * k_element(int order)
+    {
+        if(index_check(order) != 0)
+        {
+            idc temp_list;
+
+            for(int i = 0; i <= cardinal ; i++)
+            {
+                temp_list.insert((*this)[i]->value);
+            }
+
+            for(int i = 1; i < order ; i++)
+            {
+                temp_list.del(temp_list.min()->index);
+            }
+
+            for(int i = 0; i <= cardinal ; i++)
+            {
+                if((*this)[i]->value == temp_list.min()->value)
+                {
+                    return (*this)[i];
+                }
+            }
+        }else
+        {
+            cout<<"Null";
+            return nullptr;
+        }      
+    }
+
+
+    int get_cardinal()
+    {
+        return cardinal + 1;
+    }
+
+    bool inside(int value)
+    {
+        for(int i = 0; i<= cardinal;i++)
+        {
+            if((*this)[i]->value == value )
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    ///OPERATORS///
 
     friend ostream& operator << (ostream& os, const idc container)
     {
         
         element * start =  container.first;
-        //element * start = container.last;
+        //element * start = container.last;       invers
         
-        while(start->index <= container.cardinal)
+        while(start->index < container.cardinal)
         {
             os<<" "<<start->value;
-            
             start = start->next;
+            
         }
-
-        /*while(start->index >= 0)
+        os<<" "<<start->value;
+        /*while(start->index >= 0)       invers
         {
             os<<" "<<start->value;
             
             start = start->prev;
         }*/
-
+        
         return os;
     }
     
+    element* operator [](int index)
+    {
+        index_check(index);
 
+        if(index >= cardinal/2)
+        {
+            element * start =  last;
+            if(start->index == index)
+            {
+                return start;
+            }
+
+            while(start->index != index)
+            {   
+                start = start->prev;
+                if(start->index == index)
+                {
+                    return start;
+                }
+            }
+            
+        }
+        else
+        {
+            element * start =  first;
+            if(start->index == index)
+            {
+                return start;
+            }
+            while(start->index != index)
+            {   
+                start = start->next;
+                if(start->index == index)
+                {
+                    return start;
+                }
+            }
+            
+        }
+    }
 
 };
 
@@ -93,19 +332,36 @@ int main()
 {
     
     idc lista;
-    lista.insert(1);
-    lista.insert(2);
-    lista.insert(3);
-    lista.insert(4);
-    lista.insert(4);
-    lista.insert(4);
-    lista.insert(6);
-    lista.insert(5);
+    lista.insert(9);
+    lista.insert(-3);
+    lista.insert(-568);
+    lista.insert(-4);
+    lista.insert(-10);
+    lista.insert(1000);
+    lista.insert(32);
+    lista.insert(45);
+    lista.insert(-324);
+    lista.insert(40392);
+    lista.insert(-3232);
+    lista.insert(-2);
     lista.insert(62323);
-    lista.insert(122222);
-    lista.insert(12);
-    cout<<lista;
-    
+    lista.insert(-122222);
+    lista.insert(-12);
 
+
+    cout<<lista[10]->value<<endl; //afisare valoare singulara
+    cout<<lista<<endl; //afisare lista
+    //lista.del(-1); //stergere elementul de pe pozitia 3
+    cout<<endl;
+    cout<<lista[9]->value<<endl;
+    cout<<lista<<endl; //reafisare lista
+    
+    cout<<"Min: "<<lista.min()->value<<endl; //min
+    cout<<"Max: "<<lista.max()->value<<endl; //max
+    cout<<"Succesor: "<<lista.succesor(5)->value<<endl; //succesor
+    cout<<"Predecesor: "<<lista.predecessor(200)<<endl; //predecesor
+    cout<<"k-element: "<<lista.k_element(120)<<endl;  //smallest element in ascending order
+    cout<<"Inside: "<<lista.inside(-12)<<endl; //is in the list
+    cout<<"Inside: "<<lista.inside(11)<<endl;
     return 1;
 }
